4807eff83683701d6f1ffe93b68de16d9bb264e7
Tayane Cibely <tayane.cibely@ufape.edu.br>
2024-09-08 14:55:29 -0300

    Solução do exercício 1004.c produto com inteiros em c

---
+++ 1004.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    int produto;
+    scanf("%d\n%d\n", &a, &b);
+
+    produto = a * b;
+    
+    printf("PROD = %d\n", produto);
+    return 0;
+}
